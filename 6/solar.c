c165890aefd3b1c293acbfe0f1de98d2983b190f
Joe Smith <yasumoto7@gmail.com>
2009-01-26 15:15:22 -0800

    Orbiting planets. Signed-off-by: Joe Smith <yasumoto7@gmail.com>

--- 6/solar.c
+++ 6/solar.c
@@ -22,7 +22,14 @@ GLfloat upZ = 0.0;;
 
 GLfloat x_rot = 1.0, y_rot = 0.0, z_rot = 0.0;
 
+GLfloat mercury_orbit = 0.0;
+GLfloat venus_orbit = 0.0;
 GLfloat earth_orbit = 0.0;
+GLfloat mars_orbit = 0.0;
+GLfloat jupiter_orbit = 0.0;
+GLfloat saturn_orbit = 0.0;
+GLfloat uranus_orbit = 0.0;
+GLfloat neptune_orbit = 0.0;
 
 /*  Initialize material property, light source, lighting model,
  *  and depth buffer.
@@ -87,14 +94,22 @@ void display(void)
 	glPopMatrix();
 */
 
+	//The Sun
 	glPushMatrix();
 	glLoadIdentity();
 	glColor3f(1.0, 1.0, 0.0);
-	glScalef(0.5, 0.5, 0.5);
+	glScalef(0.1, 0.1, 0.1);
 	glutSolidSphere((GLdouble) 1.0, (GLint)50, (GLint)50);
 	glPopMatrix();
 
+	mercury();
+	venus();
 	earth();
+	mars();
+	jupiter();
+	saturn();
+	uranus();
+	neptune();
 
 
    glFlush ();
@@ -103,24 +118,148 @@ void display(void)
    glutSwapBuffers();
 }
 
+
+void mercury()
+{
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(1.0, 1.0, 1.0);
+	glScalef(MERCURY_SIZE);
+	glTranslatef(EARTH_DISTANCE*cos((float)mercury_orbit), EARTH_DISTANCE*sin((float)mercury_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
+void venus()
+{
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(1.0, 0.2, 0.0);
+	glScalef(VENUS_SIZE);
+	glTranslatef(VENUS_DISTANCE*cos((float)venus_orbit), VENUS_DISTANCE*sin((float)venus_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
 void earth()
 {
 	glPushMatrix();
 	glLoadIdentity();
 	glColor3f(0.0, 0.0, 1.0);
-	glScalef(0.5, 0.5, 0.5);
-	glTranslatef(2.0*cos((float)earth_orbit), 2.0*sin((float)earth_orbit), 0.0);
+	glScalef(EARTH_SIZE);
+	glTranslatef(EARTH_DISTANCE*cos((float)earth_orbit), EARTH_DISTANCE*sin((float)earth_orbit), 0.0);
 	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
 	glPopMatrix();
 }
 
-void idle()
+void mars()
 {
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(1.0, 0.0, 0.0);
+	glScalef(MARS_SIZE);
+	glTranslatef(MARS_DISTANCE*cos((float)mars_orbit), MARS_DISTANCE*sin((float)mars_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
+void jupiter()
+{
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(1.0, 5.0, 0.0);
+	glScalef(JUPITER_SIZE);
+	glTranslatef(JUPITER_DISTANCE*cos((float)jupiter_orbit), JUPITER_DISTANCE*sin((float)jupiter_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
+void saturn()
+{
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(5.0, 1.0, 0.0);
+	glScalef(SATURN_SIZE);
+	glTranslatef(SATURN_DISTANCE*cos((float)saturn_orbit), SATURN_DISTANCE*sin((float)saturn_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
+void uranus()
+{
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(0.0, 1.0, 0.0);
+	glScalef(URANUS_SIZE);
+	glTranslatef(URANUS_DISTANCE*cos((float)uranus_orbit), URANUS_DISTANCE*sin((float)uranus_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
+void neptune()
+{
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(0.0, 0.0, 1.0);
+	glScalef(NEPTUNE_SIZE);
+	glTranslatef(NEPTUNE_DISTANCE*cos((float)neptune_orbit), NEPTUNE_DISTANCE*sin((float)neptune_orbit), 0.0);
+	glutSolidSphere((GLdouble) 0.5, (GLint)50, (GLint)50);
+	glPopMatrix();
+}
+
+void orbits()
+{
+	mercury_orbit = (mercury_orbit + 0.01);
+	if (mercury_orbit >= 360.0)
+	{
+		mercury_orbit = 0.0;
+	}
+
+	venus_orbit = (venus_orbit + 0.003);
+	if (venus_orbit >= 360.0)
+	{
+		venus_orbit = 0.0;
+	}
+
 	earth_orbit = (earth_orbit + 0.001);
 	if (earth_orbit >= 360.0)
 	{
 		earth_orbit = 0.0;
 	}
+
+	mars_orbit = (mars_orbit + 0.0005);
+	if (mars_orbit >= 360.0)
+	{
+		mars_orbit = 0.0;
+	}
+
+	jupiter_orbit = (jupiter_orbit + 0.00001);
+	if (jupiter_orbit >= 360.0)
+	{
+		jupiter_orbit = 0.0;
+	}
+
+	saturn_orbit = (saturn_orbit + 0.000001);
+	if (saturn_orbit >= 360.0)
+	{
+		saturn_orbit = 0.0;
+	}
+
+	uranus_orbit = (uranus_orbit + 0.00000001);
+	if (uranus_orbit >= 360.0)
+	{
+		uranus_orbit = 0.0;
+	}
+
+	neptune_orbit = (neptune_orbit + 0.00000001);
+	if (neptune_orbit >= 360.0)
+	{
+		neptune_orbit = 0.0;
+	}
+}
+void idle()
+{
+	orbits();
 	display();
 }
 
